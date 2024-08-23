4ec0987c8e92df1cb00146f580180e3f853c635b
Aidan <am.mcdonald00@gmail.com>
2024-08-23 20:54:55 +0800

    feat: Add sign-up and sign-in pages
    This commit adds the sign-up and sign-in pages to the app. The sign-up page is a simple component that displays the text "SignUp". The sign-in page is also a simple component that displays the text "SignIn". These pages are essential for user authentication and will be further developed in future commits.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const loggedIn = { firstName: "John", lastName: "Doe" };
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+        {children} 
+    </main>
+  );
+}
